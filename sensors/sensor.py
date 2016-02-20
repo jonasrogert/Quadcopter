d0b2e66a733a64a9d80133ba1d5e3c10c03bd8d3
Jonas Rogert <jonas.rogert@britny.se>
2016-02-21 00:33:22 +0100

    meh

--- sensors/sensor.py
+++ sensors/sensor.py
@@ -131,7 +131,7 @@ class sensor(threading.Thread):
 
             if self.imulog is True:
                 self.datalog += self.getDataString(stepTime, level=0)
-                self.logger.debug(self.getDataString(stepTime)
+                self.logger.debug(self.getDataString(stepTime))
 
             #used for performance test only
             doPerf = False
