9e23d2dc913ddc90c680351fd9471d439424745c
smaaland <smaaland86@gmail.com>
2016-02-20 22:06:01 +0100

    mp6050 helper functions

--- quad/mp6050.py
+++ quad/mp6050.py
@@ -41,34 +41,56 @@ address = 0x68       # This is the address value read via the i2cdetect command
 # Now wake the 6050 up as it starts in sleep mode
 bus.write_byte_data(address, power_mgmt_1, 0)
 
-while(True):
-	time.sleep(1)
-	print "gyro data"
-	print "---------"
 
-	gyro_xout = read_word_2c(0x43)
-	gyro_yout = read_word_2c(0x45)
-	gyro_zout = read_word_2c(0x47)
+def get_gyro():
+    return [read_word_2c(0x43), read_word_2c(0x45), read_word_2c(0x47)]
 
-	print "gyro_xout: ", gyro_xout, " scaled: ", (gyro_xout / 131)
-	print "gyro_yout: ", gyro_yout, " scaled: ", (gyro_yout / 131)
-	print "gyro_zout: ", gyro_zout, " scaled: ", (gyro_zout / 131)
 
-	print
-	print "accelerometer data"
-	print "------------------"
+def get_gyro_scaled():
+    return [read_word_2c(0x43)/131, read_word_2c(0x45)/131, read_word_2c(0x47)/131]
 
-	accel_xout = read_word_2c(0x3b)
-	accel_yout = read_word_2c(0x3d)
-	accel_zout = read_word_2c(0x3f)
 
-	accel_xout_scaled = accel_xout / 16384.0
-	accel_yout_scaled = accel_yout / 16384.0
-	accel_zout_scaled = accel_zout / 16384.0
+def get_accelerometer():
+    return [read_word_2c(0x3b), read_word_2c(0x3d), read_word_2c(0x3f)]
 
-	print "accel_xout: ", accel_xout, " scaled: ", accel_xout_scaled
-	print "accel_yout: ", accel_yout, " scaled: ", accel_yout_scaled
-	print "accel_zout: ", accel_zout, " scaled: ", accel_zout_scaled
 
-	print "x rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
-	print "y rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+def get_accelerometer_scaled():
+    return [read_word_2c(0x3b)/16384.0, read_word_2c(0x3d)/16384.0, read_word_2c(0x3f)/16384.0]
+
+
+def get_rotation():
+    acc = get_accelerometer_scaled()
+    return [get_x_rotation(acc[0], acc[1], acc[2]), get_y_rotation(acc[0], acc[1], acc[2])]
+
+
+# while(True):
+# 	time.sleep(1)
+#	print "gyro data"
+#	print "---------"
+#
+#	gyro_xout = read_word_2c(0x43)
+#	gyro_yout = read_word_2c(0x45)
+#	gyro_zout = read_word_2c(0x47)
+#
+#	print "gyro_xout: ", gyro_xout, " scaled: ", (gyro_xout / 131)
+#	print "gyro_yout: ", gyro_yout, " scaled: ", (gyro_yout / 131)
+#	print "gyro_zout: ", gyro_zout, " scaled: ", (gyro_zout / 131)
+#
+#	print
+#	print "accelerometer data"
+#	print "------------------"
+#
+#	accel_xout = read_word_2c(0x3b)
+#	accel_yout = read_word_2c(0x3d)
+#	accel_zout = read_word_2c(0x3f)
+#
+#	accel_xout_scaled = accel_xout / 16384.0
+#	accel_yout_scaled = accel_yout / 16384.0
+#	accel_zout_scaled = accel_zout / 16384.0
+#
+#	print "accel_xout: ", accel_xout, " scaled: ", accel_xout_scaled
+#	print "accel_yout: ", accel_yout, " scaled: ", accel_yout_scaled
+#	print "accel_zout: ", accel_zout, " scaled: ", accel_zout_scaled
+#
+#	print "x rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+#	print "y rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
