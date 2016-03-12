41d539725ef58569e428b38cab4e42149f938939
smaaland <smaaland86@gmail.com>
2016-03-12 21:20:58 +0100

    start structure for motor file

--- quad/motor.py
+++ quad/motor.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(40, GPIO.OUT)
 GPIO.setup(36, GPIO.OUT)
@@ -7,53 +8,110 @@ GPIO.setup(32, GPIO.OUT)
 GPIO.setup(26, GPIO.OUT)
 
 dc = 5
+global_dc = 0
+dc_stepping = 0.05
 
+# TODO make sure motors are connected in the right order
+
+'''
+Motor mapping:
+01  (north)
+23  (south)
+'''
 servos = [
-    GPIO.PWM(40,50),
-    GPIO.PWM(36,50),
-    GPIO.PWM(32,50),
-    GPIO.PWM(26,50)
+    GPIO.PWM(40, 50),
+    GPIO.PWM(36, 50),
+    GPIO.PWM(32, 50),
+    GPIO.PWM(26, 50)
 ]
 for s in servos:
-    s.start(10)
-    time.sleep(5)
+    s.start(1)
+    time.sleep(1)
     print('Starting motor')
-	
 
 print('***Connect Battery & Press ENTER to start')
-res = raw_input()
-for s in servos:
-    s.ChangeDutyCycle(5)
-    time.sleep(5)
-    print('changecuty cycle for motor')
-
-print('***Press ENTER to start')
+# res = raw_input()
+# for s in servos:
+#     s.ChangeDutyCycle(5)
+#     time.sleep(5)
+#     print('change duty cycle for motor')
+#
+# print('***Press ENTER to start')
 res = raw_input()
 
 print ('increase > a | decrease > z | save Wh > n | set Wh > h|quit > 9')
 
 cycling = True
+
+
+def read_sensor_values():
+    pass
+
+
+def calculate_dc_for_motor(motor, global_dc, sensor_values):
+    adjustment = 0
+
+    # if pitch < 0 => nose points towards the ground
+    if sensor_values['pitch'] < 0:
+        # We should move the north up, and the south down
+        if motor in (0,1):
+            adjustment += dc_stepping
+        else:
+            adjustment -= dc_stepping
+    elif sensor_values['pitch'] > 0:
+        # We should move the north down, and the south up
+        if motor in (0,1):
+            adjustment -= dc_stepping
+        else:
+            adjustment += dc_stepping
+
+    # if roll < 0 => it leans to the left
+    if sensor_values['roll'] < 0:
+        # We should move the west up, and the east down
+        if motor in (0,2):
+            adjustment += dc_stepping
+        else:
+            adjustment -= dc_stepping
+    elif sensor_values['roll'] > 0:
+        # We should move the west down, and the east up
+        if motor in (0,2):
+            adjustment -= dc_stepping
+        else:
+            adjustment += dc_stepping
+
+    new_dc = global_dc + adjustment
+
+    if new_dc > 100:
+        new_dc = 100
+    elif new_dc < 0:
+        new_dc = 0
+
+    return new_dc
+
+
 try:
     while cycling:
-        for s in servos:
-	    s.ChangeDutyCycle(dc)
-        res = raw_input()
-        if res == 'a':
-            dc = dc + 0.05
-        if res == 'z':
-            dc = dc - 0.05
-        if res == 'h':
-            mymotor.setWh()
-        if res == '9':
-            cycling = False
+        # res = raw_input()
+        # if res == 'a':
+        #     global_dc += stepping
+        # if res == 'z':
+        #     global_dc -= stepping
+        # if res == '9':
+        #     cycling = False
+
+        # TODO read sensor values
+        sensor_values = read_sensor_values()
+        for motor_index, s in servos.items():
+            # TODO calculate each motors value independently
+            dc = calculate_dc_for_motor(motor_index, global_dc, sensor_values)
+            s.ChangeDutyCycle(dc)
 finally:
     # shut down cleanly
     for s in servos:
         s.stop()
 
     print ("dc var setting is: ")
-    print (dc)
-
+    print (global_dc)
 
 print('***Press ENTER to quit')
 res = raw_input()
