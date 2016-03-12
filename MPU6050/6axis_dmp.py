bd8c1caf1fe00fa0c0bcbd6a06d3a477fe383729
Jonas Rogert <jonas.rogert@britny.se>
2016-03-12 18:46:34 +0100

    meh

--- MPU6050/6axis_dmp.py
+++ MPU6050/6axis_dmp.py
@@ -16,38 +16,38 @@ packetSize = mpu.dmpGetFIFOPacketSize()
 term = Terminal()
 
 with term.fullscreen, term.position:
-while True:
-    # Get INT_STATUS byte
-    mpuIntStatus = mpu.getIntStatus()
+    while True:
+        # Get INT_STATUS byte
+        mpuIntStatus = mpu.getIntStatus()
 
-    if mpuIntStatus >= 2: # check for DMP data ready interrupt (this should happen frequently)
-        # get current FIFO count
-        fifoCount = mpu.getFIFOCount()
-
-        # check for overflow (this should never happen unless our code is too inefficient)
-        if fifoCount == 1024:
-            # reset so we can continue cleanly
-            mpu.resetFIFO()
-            print('FIFO overflow!')
-
-
-        # wait for correct available data length, should be a VERY short wait
-        fifoCount = mpu.getFIFOCount()
-        while fifoCount < packetSize:
+        if mpuIntStatus >= 2: # check for DMP data ready interrupt (this should happen frequently)
+            # get current FIFO count
             fifoCount = mpu.getFIFOCount()
 
-        result = mpu.getFIFOBytes(packetSize)
-        q = mpu.dmpGetQuaternion(result)
-        g = mpu.dmpGetGravity(q)
-        ypr = mpu.dmpGetYawPitchRoll(q, g)
+            # check for overflow (this should never happen unless our code is too inefficient)
+            if fifoCount == 1024:
+                # reset so we can continue cleanly
+                mpu.resetFIFO()
+                print('FIFO overflow!')
 
-        term.position(0,1)
-        print(ypr['yaw'] * 180 / math.pi),
-        term.position(0,2)
-        print(ypr['pitch'] * 180 / math.pi),
-        term.position(0,3)
-        print(ypr['roll'] * 180 / math.pi)
 
-        # track FIFO count here in case there is > 1 packet available
-        # (this lets us immediately read more without waiting for an interrupt)
-        fifoCount -= packetSize
+            # wait for correct available data length, should be a VERY short wait
+            fifoCount = mpu.getFIFOCount()
+            while fifoCount < packetSize:
+                fifoCount = mpu.getFIFOCount()
+
+            result = mpu.getFIFOBytes(packetSize)
+            q = mpu.dmpGetQuaternion(result)
+            g = mpu.dmpGetGravity(q)
+            ypr = mpu.dmpGetYawPitchRoll(q, g)
+
+            term.position(0,1)
+            print(ypr['yaw'] * 180 / math.pi),
+            term.position(0,2)
+            print(ypr['pitch'] * 180 / math.pi),
+            term.position(0,3)
+            print(ypr['roll'] * 180 / math.pi)
+
+            # track FIFO count here in case there is > 1 packet available
+            # (this lets us immediately read more without waiting for an interrupt)
+            fifoCount -= packetSize
