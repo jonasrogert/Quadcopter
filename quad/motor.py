0d64fe8db7def18fe6832904d3b02c011c90dd30
smaaland <smaaland86@gmail.com>
2016-03-12 22:01:58 +0100

    refactoring

--- quad/motor.py
+++ quad/motor.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import threading
+from MPU6050 import axis_dmp
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(40, GPIO.OUT)
@@ -8,45 +9,31 @@ GPIO.setup(36, GPIO.OUT)
 GPIO.setup(32, GPIO.OUT)
 GPIO.setup(26, GPIO.OUT)
 
-dc = 5
-global_dc = 0
-dc_stepping = 0.05
 
-# TODO make sure motors are connected in the right order
+class InputThread(threading.Thread):
 
-'''
-Motor mapping:
-01  (north)
-23  (south)
-'''
-servos = [
-    GPIO.PWM(40, 50),
-    GPIO.PWM(36, 50),
-    GPIO.PWM(32, 50),
-    GPIO.PWM(26, 50)
-]
-for s in servos:
-    s.start(1)
-    time.sleep(1)
-    print('Starting motor')
+    global global_dc, dc_stepping
 
-print('***Connect Battery & Press ENTER to start')
-# res = raw_input()
-# for s in servos:
-#     s.ChangeDutyCycle(5)
-#     time.sleep(5)
-#     print('change duty cycle for motor')
-#
-# print('***Press ENTER to start')
-res = raw_input()
+    def __init__(self):
+        self.running = True
+        threading.Thread.__init__(self)
 
-print ('increase > a | decrease > z | save Wh > n | set Wh > h|quit > 9')
+    def run(self):
+        while cycling:
+            res = raw_input()
+            if res == 'a':
+                global_dc += dc_stepping
+            if res == 'z':
+                global_dc -= dc_stepping
+            if res == '9':
+                cycling = False
 
-cycling = True
+    def stop(self):
+        self.running = False
 
 
 def read_sensor_values():
-    pass
+    return axis_dmp.sensor_value
 
 
 def calculate_dc_for_motor(motor, global_dc, sensor_values):
@@ -90,47 +77,73 @@ def calculate_dc_for_motor(motor, global_dc, sensor_values):
     return new_dc
 
 
-try:
-    while cycling:
-        # TODO read sensor values
-        sensor_values = read_sensor_values()
-        for motor_index, s in servos.items():
-            # TODO calculate each motors value independently
-            dc = calculate_dc_for_motor(motor_index, global_dc, sensor_values)
-            s.ChangeDutyCycle(dc)
-finally:
-    # shut down cleanly
+dc = 5
+global_dc = 0
+dc_stepping = 0.05
+
+
+def main_loop():
+
+    # TODO make sure motors are connected in the right order
+    
+    '''
+    Motor mapping:
+    01  (north)
+    23  (south)
+    '''
+    servos = [
+        GPIO.PWM(40, 50),
+        GPIO.PWM(36, 50),
+        GPIO.PWM(32, 50),
+        GPIO.PWM(26, 50)
+    ]
+
+    print('Starting sensor thread, sleeping for stabilizing')
+    axis_dmp.start_worker()
+    time.sleep(5)
+    print('Sensor thread started')
+
+    print('Starting input thread')
+    it = InputThread()
+    it.start()
+
+    for s in servos:
+        s.start(1)
+        time.sleep(1)
+        print('Starting motor')
+
+    print('***Connect Battery & Press ENTER to start')
+    res = raw_input()
+
+    print ('increase > a | decrease > z | save Wh > n | set Wh > h|quit > 9')
+
+    cycling = True
+
+    try:
+        while cycling:
+            # read sensor values
+            sensor_values = read_sensor_values()
+            for motor_index, s in servos.items():
+                # calculate each motors value independently
+                dc = calculate_dc_for_motor(motor_index, global_dc, sensor_values)
+                s.ChangeDutyCycle(dc)
+    finally:
+        # shut down cleanly
+        for s in servos:
+            s.stop()
+
+        # stop input thread
+        it.stop()
+
+        print ("dc var setting is: ")
+        print (global_dc)
+
+    print('***Press ENTER to quit')
+    res = raw_input()
     for s in servos:
         s.stop()
-
-    # TODO stop input thread
-
-    print ("dc var setting is: ")
-    print (global_dc)
-
-print('***Press ENTER to quit')
-res = raw_input()
-for s in servos:
-    s.stop()
-GPIO.cleanup()
+    GPIO.cleanup()
 
 
-class InputThread(threading.Thread):
-
-    global global_dc, dc_stepping
-    def __init__(self):
-        self.running = True
-        threading.Thread.__init__(self)
-
-    def run(self):
-        while cycling:
-            res = raw_input()
-            if res == 'a':
-                global_dc += dc_stepping
-            if res == 'z':
-                global_dc -= dc_stepping
-            if res == '9':
-                cycling = False
-
-    def stop(self):
-        self.running = False
+if __name__ == "__main__":
+    main_loop()
